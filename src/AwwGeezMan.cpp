9a9dc8077e1e455fee3b283f57d01765abfe6290
NafisulKhondaker <70341665+NafisulKhondaker@users.noreply.github.com>
2020-09-28 23:14:42 -0700

    Update AwwGeezMan.cpp

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -1,34 +1,12 @@
-/*
- _____ ____ _____   ____ _____ ____  
-| ____/ ___| ____| |___ \___  | ___| 
-|  _|| |   |  _|     __) | / /|___ \ 
-| |__| |___| |___   / __/ / /  ___) |
-|_____\____|_____| |_____/_/  |____/ 
-                                     
- _   _                                         _      _ 
-| | | | ___  _ __ ___   _____      _____  _ __| | __ / |
-| |_| |/ _ \| '_ ` _ \ / _ \ \ /\ / / _ \| '__| |/ / | |
-|  _  | (_) | | | | | |  __/\ V  V / (_) | |  |   <  | |
-|_| |_|\___/|_| |_| |_|\___| \_/\_/ \___/|_|  |_|\_\ |_|
- 
- 
-Aww Geez Man: Interdimensional Mortys.
+//Nafisul Khondaker
+//9.28.2020
 
-One of Morty C137's favorite phrases is "Aww Geez Man". But, in dimension Z286, there
-exists a Morty who says everything backwards.
-
-*/
 #include "Morty.hpp"
 
 using std::string;
 using interdimensional::args;
 
-
-int main (int ac, char** av) {
-	// Parse the command line arguments. The program is executed as
-	// ./AwwGeezMan {start} {stop} {dimension}
-	// or 
-	// ./AwwGeezMan {start} {stop} {step} {dimension}
+int main(int ac, char** av) {
 	if (ac != 4 and ac != 5) {
 		std::cout << "Error: Command line arguments are incorrect. Call program as (1) or (2)" 
 			<< std::endl;
@@ -46,11 +24,10 @@ int main (int ac, char** av) {
 		Z286::Morty(inp);
 		break;
 	default:
-		std::cout << "ERROR: Unknown dimension" << std::endl;
+		std::cout << "ERROR: Unknown dimension!!" << std::endl;
 	}
-	
-	
-	
-	
+
 	return 0;
 }
+
+
