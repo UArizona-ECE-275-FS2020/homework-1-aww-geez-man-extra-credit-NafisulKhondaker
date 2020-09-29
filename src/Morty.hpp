9269f5fd7ee2fb409c57f5363e497ec5ea933b5b
NafisulKhondaker <70341665+NafisulKhondaker@users.noreply.github.com>
2020-09-28 23:12:44 -0700

    Update Morty.hpp

--- src/Morty.hpp
+++ src/Morty.hpp
@@ -1,31 +1,22 @@
-/* 
- __  __            _               _                 
-|  \/  | ___  _ __| |_ _   _      | |__  _ __  _ __  
-| |\/| |/ _ \| '__| __| | | |     | '_ \| '_ \| '_ \ 
-| |  | | (_) | |  | |_| |_| |  _  | | | | |_) | |_) |
-|_|  |_|\___/|_|   \__|\__, | (_) |_| |_| .__/| .__/ 
-                       |___/            |_|   |_|    
-*/ 
+//Nafisul Khondaker
+//9.28.2020
 
 #include <iostream>
 #include <string>
 
-namespace interdimensional { 
-	// Declare enumeration to hold the dimensions each Morty is from
-	enum dimension {UNKNOWN = -1, C137, Z286};
-	
-	// Declare a structure to hold the arguments passed to the function
+namespace interdimensional {
+	enum dimension { UNKNOWN = -1, C137, Z286 };
+
 	struct args {
 		int start;
 		int stop;
 		int step;
 		dimension dim;
 	};
-	
-	// Declare a function to parse the args and return the above declared structure
 	args parseArgs(int c, char** v);
-	
+
 }
+
 namespace C137 {
 	void Morty(interdimensional::args);
 }
